2200c6a0c28c6c0953619b449fba76f1f3bc1136
Nathan Mattes <hallo@bullenscheisse.de>
2020-02-07 14:24:06 +0100

    Initial commit

---
+++ CalendarPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalendarPOC
+//
+//  Created by Nathan Mattes on 07.02.20.
+//  Copyright © 2020 Nathan Mattes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+#  Calendar Proof of Concept
+
+This is basically a playground to play around with [SwiftPM](https://github.com/apple/swift-package-manager) and evaluate [JTAppleCalendar](https://github.com/patchthecode/JTAppleCalendar) for a project. Maybe it is helpful to you, too.
