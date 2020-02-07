264fee6c3a6bc4b62bddb74c1e296138da5fad00
Nathan Mattes <hallo@bullenscheisse.de>
2020-02-07 14:40:00 +0100

    Setup basic VC and show in a NavigationController
    Oh, UI is entirely in code, btw. Hope it doesn't bother you :D

--- CalendarPOC/AppDelegate.swift
+++ CalendarPOC/AppDelegate.swift
@@ -10,9 +10,21 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
+        self.window = window
+        
         return true
     }
 }
--- CalendarPOC/ViewController.swift
+++ CalendarPOC/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UIViewController Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .orange
     }
-
-
 }
 
